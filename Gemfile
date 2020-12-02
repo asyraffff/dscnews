720ce65eaccda87f46d66bb89285cd924cdb3238
asyraf-labs <amirulasyraf4.0x@gmail.com>
2020-12-02 14:54:37 +0800

    Add model and CRUD for views

--- Gemfile
+++ Gemfile
@@ -22,6 +22,9 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 gem 'simple_form', '~> 5.0', '>= 5.0.3'
+gem 'bootstrap-sass', '~> 3.4', '>= 3.4.1'
+gem 'sassc-rails', '>= 2.1.0'
+gem 'jquery-rails'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,13 +1,52 @@
 class LinksController < ApplicationController
+before_action :find_link, only: [:show, :edit, :update, :destroy]
     def index
-        
+        @links = Link.all.order("created_at DESC")
     end
 
     def new
-        
+        @link = Link.new
     end
 
     def create
-        
+        @link = Link.new(link_params)
+        if @link.save
+            redirect_to root_path, notice: "Succesfully created link"
+        else
+            return "new"
+        end
+    end
+
+    def show
+        # @link = Link.find(params[:id])
+    end
+
+    def edit
+        # @link = Link.find(params[:id])
+    end
+
+    def update
+        # @link = Link.find(params[:id])
+        if @link.update(link_params)
+            redirect_to link_path(@link), notice: "Succesfully updated link"
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        # @link = Link.find(params[:id])
+        @link.destroy
+
+        redirect_to root_path, notice: "Succesfully deleted link"
+    end
+
+    private
+    def link_params
+        params.require(:link).permit(:title, :url)
+    end
+
+    def find_link
+        @link = Link.find(params[:id])
     end
 end
